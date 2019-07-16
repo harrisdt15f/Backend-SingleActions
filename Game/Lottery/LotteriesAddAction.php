47e0e49d823b19c791723dca5dc7b7296d277611
LingPh <953909858@qq.com>
2019-07-16 15:10:57 +0800

    添加彩种时，自动添加玩法

--- Game/Lottery/LotteriesAddAction.php
+++ Game/Lottery/LotteriesAddAction.php
@@ -5,6 +5,8 @@ namespace App\Http\SingleActions\Backend\Game\Lottery;
 use App\Http\Controllers\backendApi\BackEndApiMainController;
 use App\Models\Game\Lottery\LotteryIssueRule;
 use App\Models\Game\Lottery\LotteryList;
+use App\Models\Game\Lottery\LotteryMethod;
+use App\Models\Game\Lottery\LotteryMethodsExample;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
@@ -27,6 +29,11 @@ class LotteriesAddAction
             DB::rollback();
             return $contll->msgOut(false, [], '400', $lotteryEloq->errors()->messages());
         }
+        $insertStatus = $this->insertLotteryMethods($lotteryEloq); //插入彩种玩法
+        if ($insertStatus['success'] === false) {
+            DB::rollback();
+            return $contll->msgOut(false, [], '400', $insertStatus['message']);
+        }
         $issueRuleELoq = new LotteryIssueRule();
         $issueRuleELoq->fill($inputDatas['issue_rule']);
         $issueRuleELoq->save();
@@ -37,5 +44,39 @@ class LotteriesAddAction
         DB::commit();
         $lotteryEloq->lotteryInfoCache(); //更新首页lotteryInfo缓存
         return $contll->msgOut(true);
+    }
+
+    /**
+     * 插入彩种的玩法
+     * @param  $lotteryEloq
+     * @return array
+     */
+    public function insertLotteryMethods($lotteryEloq): array
+    {
+        $examplesEloq = LotteryMethodsExample::where('series_id', $lotteryEloq->series_id)->get();
+        foreach ($examplesEloq as $exampleEloq) {
+            $data = [
+                'series_id' => $lotteryEloq->series_id,
+                'lottery_name' => $lotteryEloq->cn_name,
+                'lottery_id' => $lotteryEloq->en_name,
+                'method_id' => $exampleEloq->method_id,
+                'method_name' => $exampleEloq->method_name,
+                'method_group' => $exampleEloq->method_group,
+                'method_row' => $exampleEloq->method_row,
+                'group_sort' => $exampleEloq->group_sort,
+                'row_sort' => $exampleEloq->row_sort,
+                'method_sort' => $exampleEloq->method_sort,
+                'show' => $exampleEloq->show,
+                'status' => $exampleEloq->status,
+                'total' => $exampleEloq->total,
+            ];
+            $lotteryMethodEloq = new LotteryMethod();
+            $lotteryMethodEloq->fill($data);
+            $lotteryMethodEloq->save();
+            if ($lotteryMethodEloq->errors()->messages()) {
+                return ['success' => false, 'message' => $lotteryMethodEloq->errors()->messages()];
+            }
+        }
+        return ['success' => true];
     }
 }
