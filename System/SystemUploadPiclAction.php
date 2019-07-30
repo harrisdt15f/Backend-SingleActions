aa7e558b447ba23340717b598c90253659d81732
LingPh <953909858@qq.com>
2019-07-30 14:21:57 +0800

    公用上传图片接口

---
+++ System/SystemUploadPiclAction.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\SingleActions\Backend\System;
+
+use App\Http\Controllers\backendApi\BackEndApiMainController;
+use App\Lib\Common\ImageArrange;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+class SystemUploadPiclAction
+{
+
+    /**
+     * 图片上传
+     * @param  BackEndApiMainController  $contll
+     * @param  $inputDatas
+     * @return JsonResponse
+     */
+    public function execute(BackEndApiMainController $contll, $inputDatas): JsonResponse
+    {
+        $imageObj = new ImageArrange();
+        $file = $inputDatas['pic'];
+        $folderName = $inputDatas['folder_name'];
+        $depositPath = $imageObj->depositPath($folderName, $contll->currentPlatformEloq->platform_id, $contll->currentPlatformEloq->platform_name);
+        //进行上传
+        $pic = $imageObj->uploadImg($file, $depositPath);
+        if ($pic['success'] === false) {
+            return $contll->msgOut(false, [], '400', $pic['msg']);
+        }
+        //设置图片过期时间6小时
+        $pic['expire_time'] = Carbon::now()->addHours(6)->timestamp;
+        $hourToStore = 24 * 2;
+        $expiresAt = Carbon::now()->addHours($hourToStore);
+        if (Cache::has('cache_pic')) {
+            $cachePic = Cache::get('cache_pic');
+        }
+        $cachePic[$pic['name']] = $pic;
+        Cache::put('cache_pic', $cachePic, $expiresAt);
+        return $contll->msgOut(true, $pic);
+    }
+
+}
