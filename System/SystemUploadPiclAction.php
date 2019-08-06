7327a9923cdfe876fe6828780ae9531cfd6ff368
jeff <jeff8979@outlook.com>
2019-08-06 22:26:46 +0800

    新加后台接口获取头像，设定头像，和上传接口解除缓存删除

--- System/SystemUploadPiclAction.php
+++ System/SystemUploadPiclAction.php
@@ -4,16 +4,19 @@ namespace App\Http\SingleActions\Backend\System;
 
 use App\Http\Controllers\backendApi\BackEndApiMainController;
 use App\Lib\Common\ImageArrange;
+use function Couchbase\defaultDecoder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 
 class SystemUploadPiclAction
 {
+    //头像上传文件夹
+    public const USER_PUBLIC_AVATARS = "avatars";
 
     /**
      * 图片上传
-     * @param  BackEndApiMainController  $contll
+     * @param  BackEndApiMainController $contll
      * @param  $inputDatas
      * @return JsonResponse
      */
@@ -22,11 +25,18 @@ class SystemUploadPiclAction
         $imageObj = new ImageArrange();
         $file = $inputDatas['pic'];
         $folderName = $inputDatas['folder_name'];
-        $depositPath = $imageObj->depositPath($folderName, $contll->currentPlatformEloq->platform_id, $contll->currentPlatformEloq->platform_name);
+        if ($folderName == self::USER_PUBLIC_AVATARS) {
+            $depositPath = $imageObj->depositPathAvatars($folderName);
+        } else {
+            $depositPath = $imageObj->depositPath($folderName, $contll->currentPlatformEloq->platform_id, $contll->currentPlatformEloq->platform_name);
+        }
         $pic = $imageObj->uploadImg($file, $depositPath);
         if ($pic['success'] === false) {
             return $contll->msgOut(false, [], '400', $pic['msg']);
         }
+        //上传头像不记录清除日期
+        if ($folderName == self::USER_PUBLIC_AVATARS)
+            return $contll->msgOut(true, $pic);
         $pic['expire_time'] = Carbon::now()->addHours(6)->timestamp; //设置图片过期时间6小时
         $hourToStore = 24 * 2;
         $expiresAt = Carbon::now()->addHours($hourToStore);
@@ -35,6 +45,7 @@ class SystemUploadPiclAction
         }
         $cachePic[$pic['name']] = $pic;
         Cache::put('cache_pic', $cachePic, $expiresAt);
+
         return $contll->msgOut(true, $pic);
     }
 
