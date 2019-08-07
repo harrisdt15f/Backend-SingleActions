5ed46a5a1fdb6e9e8fda90ddefd78fe5283171f2
jeff <jeff8979@outlook.com>
2019-08-07 15:24:45 +0800

    更改上传文件夹存放位置

--- System/SystemUploadPiclAction.php
+++ System/SystemUploadPiclAction.php
@@ -4,6 +4,7 @@ namespace App\Http\SingleActions\Backend\System;
 
 use App\Http\Controllers\backendApi\BackEndApiMainController;
 use App\Lib\Common\ImageArrange;
+use App\Models\User\UserPublicAvatar;
 use function Couchbase\defaultDecoder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Carbon;
@@ -25,18 +26,17 @@ class SystemUploadPiclAction
         $imageObj = new ImageArrange();
         $file = $inputDatas['pic'];
         $folderName = $inputDatas['folder_name'];
-        if ($folderName == self::USER_PUBLIC_AVATARS) {
-            $depositPath = $imageObj->depositPathAvatars($folderName);
-        } else {
-            $depositPath = $imageObj->depositPath($folderName, $contll->currentPlatformEloq->platform_id, $contll->currentPlatformEloq->platform_name);
-        }
+        $depositPath = $imageObj->depositPath($folderName, $contll->currentPlatformEloq->platform_id, $contll->currentPlatformEloq->platform_name);
         $pic = $imageObj->uploadImg($file, $depositPath);
         if ($pic['success'] === false) {
             return $contll->msgOut(false, [], '400', $pic['msg']);
         }
         //上传头像不记录清除日期
-        if ($folderName == self::USER_PUBLIC_AVATARS)
+        if ($folderName == self::USER_PUBLIC_AVATARS) {
+            $newPubliData = ['pic_path' => $pic['path']];
+            UserPublicAvatar::create($newPubliData);
             return $contll->msgOut(true, $pic);
+        }
         $pic['expire_time'] = Carbon::now()->addHours(6)->timestamp; //设置图片过期时间6小时
         $hourToStore = 24 * 2;
         $expiresAt = Carbon::now()->addHours($hourToStore);
