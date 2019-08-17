441cde1be8af524bd2a05aa72d38510887176111
jeff <jeff8979@outlook.com>
2019-08-17 20:59:38 +0800

    开设用户数和获取下级用户功能实现

--- Users/UserHandleUsersInfoAction.php
+++ Users/UserHandleUsersInfoAction.php
@@ -5,13 +5,14 @@ namespace App\Http\SingleActions\Backend\Users;
 use App\Http\Controllers\BackendApi\BackEndApiMainController;
 use App\Models\User\FrontendUser;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class UserHandleUsersInfoAction
 {
     protected $model;
 
     /**
-     * @param  FrontendUser  $frontendUser
+     * @param  FrontendUser $frontendUser
      */
     public function __construct(FrontendUser $frontendUser)
     {
@@ -20,7 +21,7 @@ class UserHandleUsersInfoAction
 
     /**
      * 用户管理的所有用户信息表
-     * @param  BackEndApiMainController  $contll
+     * @param  BackEndApiMainController $contll
      * @return JsonResponse
      */
     public function execute(BackEndApiMainController $contll): JsonResponse
@@ -37,10 +38,81 @@ class UserHandleUsersInfoAction
             'prize_group',
             'level_deep',
             'register_ip',
+            'parent_id',
         ];
         $withSearchAbleFields = ['balance'];
-        $data = $contll->generateSearchQuery($this->model, $searchAbleFields, $fixedJoin, $withTable,
-            $withSearchAbleFields);
+        $data = $contll->generateSearchQuery(
+            $this->model,
+            $searchAbleFields,
+            $fixedJoin,
+            $withTable,
+            $withSearchAbleFields
+        ) ;
+        $isSetTotal = isset($contll->inputs['total_members']) ? true : false;
+        $isSetParentName = isset($contll->inputs['parent_name']) ? true : false;
+
+        if ($isSetTotal || $isSetParentName) {
+            //要名字
+            if ($isSetParentName) {
+                if (count($data) > 0) {
+                    //待功能验收后进一步优化
+                    $totalData = $this->model->get()->toArray();
+                    $totalData = array_combine(array_column($totalData, 'id'), $totalData);
+                }
+            }
+            //要总数
+            if ($isSetTotal) {
+                $dataChange = $data->toArray()['data'];
+                $dataChange = array_combine(array_column($dataChange, 'id'), $dataChange);
+                $dataChange = array_keys($dataChange);
+                $specData = DB::table('frontend_users_specific_infos')
+                    ->whereIn('user_id', $dataChange)
+                    ->get()->toArray();
+                $specData = array_combine(array_column($specData, 'user_id'), $specData);
+            }
+            foreach ($data as $k => $v) {
+                if ($isSetParentName) {
+                    $parentName = self::getParentUserName($totalData, $v);
+                    if ($parentName['status'] === false) {
+                        return $contll->msgOut(false, $parentName['data'], '100110');
+                    }
+                    $data[$k]['parent_username'] = $parentName['data'];
+                }
+                if ($isSetTotal) {
+                    $data[$k]['total_members'] = $specData[$v->id]->total_members;
+                }
+            }
+        }
         return $contll->msgOut(true, $data);
+    }
+
+    /**
+     * 将用户的的父ID变为用户名
+     * @param  $totalData
+     * @param  $parentKeys
+     * @return Array
+     */
+    public function getParentUserName($totalData, $parentKeys): Array
+    {
+        $res = array(
+            'status' => true,
+            'data' => ''
+        );
+        $arr = explode("|", trim($parentKeys['rid'], '|'));
+        $parentUserName = '';
+        if (count($arr) < 1) {
+            return $res;
+        }
+        foreach ($arr as $v1) {
+            if (!isset($totalData[$v1])) {
+                $res['status'] = false;
+                $res['data'] = $parentKeys;
+                $res['error'] = '该用户的上级ID不存在,请联系管理员';
+                return $res;
+            }
+            $parentUserName .= ',' . $totalData[$v1]['username'];
+        }
+        $res['data'] = trim($parentUserName, ',');
+        return $res;
     }
 }
