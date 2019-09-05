6db5022ae666f5556772465f72dce67135b47027
jeff <jeff8979@outlook.com>
2019-09-05 21:33:41 +0800

    查询条件增加parent_id的查询条件

--- Users/UserHandleUsersInfoAction.php
+++ Users/UserHandleUsersInfoAction.php
@@ -47,72 +47,89 @@ class UserHandleUsersInfoAction
             $fixedJoin,
             $withTable,
             $withSearchAbleFields
-        ) ;
+        );
         $isSetTotal = isset($contll->inputs['total_members']) ? true : false;
         $isSetParentName = isset($contll->inputs['parent_name']) ? true : false;
+        $isSetParentId = isset($contll->inputs['parent_id']) ? true : false;
+        $isSetParentInfo = isset($contll->inputs['parent_info']) ? true : false;
 
-        if ($isSetTotal || $isSetParentName) {
-            //要名字
-            if ($isSetParentName) {
-                if (count($data) > 0) {
-                    //待功能验收后进一步优化
-                    $totalData = $this->model->get()->toArray();
-                    $totalData = array_combine(array_column($totalData, 'id'), $totalData);
-                }
-            }
-            //要总数
-            if ($isSetTotal) {
-                $dataChange = $data->toArray()['data'];
-                $dataChange = array_combine(array_column($dataChange, 'id'), $dataChange);
-                $dataChange = array_keys($dataChange);
-                $specData = DB::table('frontend_users_specific_infos')
-                    ->whereIn('user_id', $dataChange)
-                    ->get()->toArray();
-                $specData = array_combine(array_column($specData, 'user_id'), $specData);
-            }
-            foreach ($data as $k => $v) {
-                if ($isSetParentName) {
-                    $parentName = self::getParentUserName($totalData, $v);
-                    if ($parentName['status'] === false) {
-                        return $contll->msgOut(false, $parentName['data'], '100110');
-                    }
-                    $data[$k]['parent_username'] = $parentName['data'];
-                }
-                if ($isSetTotal) {
-                    $data[$k]['total_members'] = $specData[$v->id]->total_members;
-                }
-            }
+        if ($isSetParentName === true) {
+            self::getParentName($data);
+        }
+
+        if ($isSetTotal === true) {
+            self::getTotal($data);
+        }
+        if ($isSetParentId === true && $isSetParentInfo === true) {
+            self::addParentInfo($data, $contll);
         }
         return $contll->msgOut(true, $data);
     }
 
+    private function addParentInfo(&$data, $contll)
+    {
+        $parentInfo = $this->model->find($contll->inputs['parent_id'])->toArray();
+        unset($parentInfo['password']);
+        unset($parentInfo['fund_password']);
+        $data['parentInfo'] = $parentInfo;
+    }
+
+    private function getParentName(&$data)
+    {
+        $totalData = $this->model->get()->toArray();
+        $totalData = array_combine(array_column($totalData, 'id'), $totalData);
+        foreach ($data as $dataKey => $totalValue) {
+            $parentName = self::getParentUserName($totalData, $totalValue);
+            if ($parentName['status'] === false) {
+                //    return $contll->msgOut(false, $parentName['data'], '100110');
+                $data[$dataKey]['parent_username'] = '该用户上级不存在';
+            }
+            $data[$dataKey]['parent_username'] = $parentName['data'];
+        }
+    }
+
+    private function getTotal(&$data)
+    {
+        $dataChange = $data->toArray()['data'];
+        $dataChange = array_combine(array_column($dataChange, 'id'), $dataChange);
+        $dataChange = array_keys($dataChange);
+        $specData = DB::table('frontend_users_specific_infos')
+            ->whereIn('user_id', $dataChange)
+            ->get()->toArray();
+        $specData = array_combine(array_column($specData, 'user_id'), $specData);
+        foreach ($data as $dataKey => $totalValue) {
+            $data[$dataKey]['total_members'] = isset($specData[$totalValue->id]) ? $specData[$totalValue->id]->total_members : 0;
+        }
+    }
+
+
     /**
      * 将用户的的父ID变为用户名
-     * @param  $totalData
-     * @param  $parentKeys
-     * @return Array
+     * @param  mixed $totalData
+     * @param  mixed $parentKeys
+     * @return mixed
      */
-    public function getParentUserName($totalData, $parentKeys): Array
+    public function getParentUserName($totalData, $parentKeys)
     {
-        $res = array(
+        $resp = array(
             'status' => true,
             'data' => ''
         );
-        $arr = explode("|", trim($parentKeys['rid'], '|'));
+        $arrData = explode('|', trim($parentKeys['rid'], '|'));
         $parentUserName = '';
-        if (count($arr) < 1) {
-            return $res;
+        if (count($arrData) < 1) {
+            return $resp;
         }
-        foreach ($arr as $v1) {
-            if (!isset($totalData[$v1])) {
-                $res['status'] = false;
-                $res['data'] = $parentKeys;
-                $res['error'] = '该用户的上级ID不存在,请联系管理员';
-                return $res;
+        foreach ($arrData as $arrDataV) {
+            if (!isset($totalData[$arrDataV])) {
+                $resp['status'] = false;
+                $resp['data'] = $parentKeys;
+                $resp['error'] = '该用户的上级ID不存在,请联系管理员';
+                return $resp;
             }
-            $parentUserName .= ',' . $totalData[$v1]['username'];
+            $parentUserName .= ',' . $totalData[$arrDataV]['username'];
         }
-        $res['data'] = trim($parentUserName, ',');
-        return $res;
+        $resp['data'] = trim($parentUserName, ',');
+        return $resp;
     }
 }
